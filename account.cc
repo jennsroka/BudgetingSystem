65a003cc3ab3794318bdfd239b8bc9930a2dbc04
jennsroka <js922516@ohio.edu>
2018-10-08 11:47:29 -0400

    initial account.cc

---
+++ account.cc
@@ -0,0 +1,40 @@
+#ifndef ACCOUNT_CC
+#define ACCOUNT_CC
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std; 
+
+class account
+{
+	public:
+		account()
+		{
+			first = "";
+			last = "";
+		}
+		void set_name(string f, string l)
+		{
+			first = f;
+			last = l; 
+		}
+		string get_name()
+		{
+			return first + " " + last;
+		}
+		void set_pin(string p)
+		{
+			pin = p; 
+		}
+		string get_pin()
+		{
+			return pin; 
+		}
+
+	private:
+		string first, last;  
+		string pin; 
+
+};
+#endif
