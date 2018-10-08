b542813b16af5653950b7eeb5b7f9cb3cbfd217c
Guijiaxing <43178772+Guijiaxing@users.noreply.github.com>
2018-10-08 11:35:33 -0400

    0.00001 draft

---
+++ main.cc
@@ -0,0 +1,22 @@
+#include "system.h"
+
+
+using namespace std;
+
+int main{
+	int option;
+	int key;
+	do{
+		cout<<"please typing your choose:";
+		cin>>option;
+		switch(option){
+			case 1:
+			//insert();
+			break;
+			case 2:
+			//remove();
+			break;
+}	while(option != 0);
+}
+	return 0;
+}
