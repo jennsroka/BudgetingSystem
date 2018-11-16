d25f791dd6d0719fb1c50ef3f424e9b22d6b7d46
jennsroka <js922516@ohio.edu>
2018-11-16 11:26:51 -0500

    Documentation for account and bank classes

--- account.cc
+++ account.cc
@@ -1,3 +1,8 @@
+/**
+    @file account.cc
+    @brief This file contains the basic account information for the main.
+ **/
+
 #ifndef ACCOUNT_CC
 #define ACCOUNT_CC
 
@@ -9,25 +14,47 @@ using namespace std;
 class account
 {
 	public:
+        /** @brief This function is the constructor that sets all of the variables to
+            empty strings.
+         **/
 		account()
 		{
 			first = "";
 			last = "";
 			pin = "";
 		}
+    
+    /**
+        @brief This function allows the user to set the name on their account.
+        @param f - string that represents the first name
+        @param l - string that represents the last name
+     **/
 		void set_name(string f, string l)
 		{
 			first = f;
 			last = l; 
 		}
+    
+    /**
+        @brief This function returns the user's full name based on the values assigned to the strings.
+     **/
 		string get_name()
 		{
 			return first + " " + last;
 		}
+    
+    /**
+        @brief This function allows the user to set the pin that gives them access to their accounts
+        @param p - a string that is being used to set the value of the pin variable.
+     **/
 		void set_pin(string p)
 		{
 			pin = p; 
 		}
+    
+    /**
+        @brief This function returns the user's pin based on the values assigned to teh strings
+     **/
 		string get_pin()
 		{
 			return pin; 
--- bank.cc
+++ bank.cc
@@ -1,3 +1,8 @@
+/**
+    @file bank.cc
+    @brief This file defines all of the functions and variables of the bank class.
+ **/
+
 #ifndef BANK_CC
 #define BANK_CC
 #include <iostream>
@@ -8,14 +13,27 @@ using namespace std;
 class bank
 {
 	public:
+        /**
+         @brief The constructor for the bank class. Sets the original balance variable to 0.
+        **/
 		bank()
 		{
 			balance = 0; 
 		}
+    
+        /**
+            @brief This function allows the user to add to how much money is in their account.
+            @param money - a double that holds the value of how much they are adding to their account.
+         **/
 		void deposit(double money)
 		{
 			balance += money; 
 		}
+    
+        /**
+            @brief This function allows the user to subtract from the amount of money they have in their account.
+            @param money - a double that holds the value of how much they are taking out of their account.
+         **/
 		void withdraw(double money)
 		{
 			if(isFunds(money))
@@ -25,6 +43,11 @@ class bank
 			else
 				cout << "Insufficient Funds." << endl; 
 		}
+    
+        /**
+            @brief This function tests to see if the user has the proper amount of money in their account to see if there are sufficient funds for this withdrawal.
+            @param money - a double that holds the value of the amount that the function is testing to see if the balance is larger or smaller than.
+         **/
 		bool isFunds(double money)
 		{
 			if(balance >= money)
@@ -32,6 +55,10 @@ class bank
 			else 
 				return false; 
 		}
+    
+        /**
+            @brief This function returns the current value of the balance variable.
+         **/
 		double getBalance()
 		{
 			return balance;
